3fcb0d1f6acc205d9ce32fd0517049c16941c72e
anderpenagos <afpr44@pm.me>
2026-02-13 12:18:50 -0300

    fix: corrige ausencia de api

--- app/src/main/kotlin/com/runapp/ui/viewmodel/CorridaViewModel.kt
+++ app/src/main/kotlin/com/runapp/ui/viewmodel/CorridaViewModel.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 data class CorridaUiState(
